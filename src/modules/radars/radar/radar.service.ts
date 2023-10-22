36e0f9f3eefac2f6471c71b366745a298023a2a3
Hemin Johnson <achayan1992@gmail.com>
2023-10-22 14:21:31 +0200

    chore: mirror src october 22

--- src/modules/radars/radar/radar.service.ts
+++ src/modules/radars/radar/radar.service.ts
@@ -1,12 +1,39 @@
-import { Injectable } from '@nestjs/common';
-import { Radar } from './entities/radar.entity';
-import { radar } from '../../../database/mocks/radar';
+import { Injectable } from "@nestjs/common";
+import { Radar } from "./entities/radar.entity";
 
 @Injectable()
 export class RadarService {
-  private radar: Radar = radar;
+  findAll(): Radar {
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth(); // Month is zero-based (0-11)
 
-  findAll() {
-    return this.radar;
+    // Calculate the current quarter and year
+    const currentQuarter = Math.floor(currentMonth / 3) + 1; // Quarters are 1-based (1-4)
+    const currentYear = currentDate.getFullYear();
+
+    const trends = [
+      {
+        change: 'neutral',
+        item: {
+          id: 18,
+          name: 'YAML',
+          description:
+            'Yet another modeling language, popular for configurations or specifications',
+          category: {
+            id: 3,
+            name: 'Methods, Standards and Architectures',
+            description:
+              'How are we achieving an objective? (e.g. SAFe Framwork, Sustainable architecture, agile software development, continuous delivery, continuous testing, Mikroservices)',
+          },
+        },
+        value: 'assess',
+        timestamp: '2023-08-31T10:15:30.123Z',
+      },
+    ];
+
+    return {
+      name: `Radar Q${currentQuarter}/${currentYear}`,
+      trends,
+    };
   }
 }
--- src/modules/radars/votes/entities/vote.entity.ts
+++ src/modules/radars/votes/entities/vote.entity.ts
@@ -1,8 +1,8 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document } from 'mongoose';
-import { Item } from "../../items/entities/item.entity";
+import { Item } from '../../items/entities/item.entity';
 
-@Schema()
+@Schema({ timestamps: true })
 export class Vote extends Document {
   @Prop()
   value: string;
